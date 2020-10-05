f20a661d2e3bebb525127c705d410ac16eb0f33f
samsmc <samantha.martins@gmail.com>
2020-10-05 12:09:04 +0200

    done

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,37 +1,65 @@
 // Write a function that returns the product of 2 numbers
 function product(x, y) {
-  //
+  return x * y;
 }
+product(10,5);
 
 // Write a function that returns whether a given number is even
+
 function isEven(num) {
-  //
+  return %2 === 0;
 }
 
+
 // Return the largest of 2 numbers
+
 function maxOfTwoNumbers(a, b) {
-  //
+  if (a>b){
+    return a;
+  }else{
+    return b;
+    }
 }
 
 // Return the largest of 3 numbers
 function maxOfThreeNumbers(a, b, c) {
-  //
+  return Math.max(a,b,c);
 }
 
 // Calculate the sum of an array of numbers
-function sumArray(numbers) {
-  //
+var array = [1,2,3,4,5];
+var sum = array.reduce(function(a,b){
+  return a + b;
+}, 0);
+console.log(sum);
+
+
+//function sumArray(numbers) {
+  if(number.length === 0){
+    return 0
+  }
+  return numbers.reduce((acc,currentValue) => acc + currentValue)
 }
 
 // Return the largest number of a non-empty array
 function maxOfArray(numbers) {
-  //
+  if (numbers.length === 0) {
+    return 0
+  }
+  return Math.max(numbers);
 }
 
 // Return the longest string in an array
 function longestString(strings) {
-  //
+  var count1 = string[0];
+  for (let i = 0; i<strings.lengh; i++){
+    if(strings[i].length>count1.length){
+      count = strings[i];
+    }
+  }
+  return count1;
 }
+console.log(longestString(strings));
 
 // Return whether a word is in an array
 function doesWordExist(wordsArr, word) {
